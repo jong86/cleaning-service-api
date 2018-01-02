9d1d89c142c570b8d2e9fe61f3d488a8990a25a8
Jazmin <Jazmin@Jons-iMac.local>
2018-01-01 17:23:00 -0800

    Initial commit

---
+++ db/migrate/20180102011519_create_employees.rb
@@ -0,0 +1,12 @@
+class CreateEmployees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Employee.create(
+  name: 'Fake Person',
+  address: '123 Fake St.',
+  phone_number: '123-456-7890',
+  email: 'fake@fake.fake',
+)
