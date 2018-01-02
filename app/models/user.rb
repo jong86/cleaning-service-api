63b4759169c6eca252d38aebab122dbc99583e09
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-01 23:21:39 -0800

    Added authentication with JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :job_requests, class_name: "JobRequest", foreign_key: "client_id"
   has_many :jobs, through: :job_requests
   has_many :jobs, class_name: "Job", foreign_key: "employee_id"
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :clients
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'authenticate', to: 'authentication#authenticate'
 end
