19e8e323e803aed937c6c5ad25e3711897bfbb0c
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-02 22:23:19 -0800

    Added destroy method to JobRequestController, with user validation

--- app/controllers/job_requests_controller.rb
+++ app/controllers/job_requests_controller.rb
@@ -9,6 +9,19 @@ class JobRequestsController < ApplicationController
     end
   end
 
+
+  def destroy
+    client_id_of_job_request = JobRequest.find(params[:id]).client_id
+    
+    if current_user[:id].to_i == client_id_of_job_request.to_i
+      JobRequest.delete(params[:id])
+      render json: { message: "Job request ##{params[:id]} has been deleted." }, status: 200
+    else
+      render json: { message: "You are not allowed to delete other people's data." }, status: 403
+    end
+  end
+
+
   def index
     if current_user[:id].to_i == params[:client_id].to_i
       render json: { job_requests: User.find(params[:client_id]).job_requests }, status: 200
@@ -17,16 +30,29 @@ class JobRequestsController < ApplicationController
     end
   end
   
+
   def show
     client_id_of_job_request = JobRequest.find(params[:id]).client_id
+
     if current_user[:id].to_i == client_id_of_job_request.to_i
       job_request = JobRequest.find(params[:id])
       render json: {
         job_request: job_request,
-        client: job_request.client,
-      }, status: 200
+        }, status: 200
     else
       render json: { message: "Users can only view their own information." }, status: 403
+    end
+  end
+    
+
+  def update
+    client_id_of_job_request = JobRequest.find(params[:id]).client_id
+
+    if current_user[:id].to_i == client_id_of_job_request.to_i
+      JobRequest.update(params[:id], filtered_params)
+      render json: { message: "Job request updated." }, status: 200
+    else
+      render json: { message: "You cannot update another user's data." }, status: 403
     end
   end
 
