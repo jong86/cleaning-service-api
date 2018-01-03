eaa6ae3059f858a5f6c6460baecbbe8bb146df7b
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-03 14:22:08 -0800

    Added email confirmation and filter_params to clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,12 @@ class ClientsController < ApplicationController
   skip_before_action :authenticate_request, only: :create
 
   def create
-    render json: { message: "create path for client" }, status: 200
+    if params[:email] == params[:email_confirmation]
+      render json: { message: "Creating a new client with this info: #{filtered_params}" }, status: 200
+      Client.create(filtered_params)
+    else
+      render json: { message: "Email and email confirmation don't match." }, status: 400
+    end
   end
 
 
@@ -13,5 +18,20 @@ class ClientsController < ApplicationController
       job_requests: @client.job_requests,
       jobs: @client.jobs,
     }, status: 200
+  end
+
+
+  private
+
+  def filtered_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+      :phone_number,
+      :address,
+      :info,
+    )
   end
 end
