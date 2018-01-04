9b5edb05e3c0661c946046b35f92c27eb371f983
Jon Gaspar <jon@Jons-MacBook-Pro.local>
2018-01-04 14:46:57 -0800

    admin/employees#create not makes an employee and returns employee data

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,17 +1,48 @@
 class Admin::EmployeesController < ApplicationController
-  def index
-    render json: { message: Employee.all }, status: 200
+  def create
+    employee = Employee.create!(filtered_params)
+    render json: {
+      message: "Employee created",
+      employee: employee, 
+    }, status: 200
   end
   
+  def index
+    render json: {
+      message: "Rendering list of all employees",
+      employees: Employee.all
+    }, status: 200
+  end
+
   def jobs
+    # For returning a list of all of an employee's jobs
+    # (maybe not needed with AR but couldn't get it to work normally)
     Job.where("employee_id = ?", self.id)
   end
 
   def show
-    @employee = Employee.find(params[:id])
+    employee = Employee.find(params[:id])
     render json: {
-      user_data: @employee,
-      jobs: @employee.jobs,
+      user_data: employee,
+      jobs: employee.jobs,
     }, status: 200
+  end
+
+
+  private
+
+  def filtered_params
+    params.permit(
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation,
+      :phone_number,
+      :address,
+      :availability,
+      :info,
+    )
   end
 end
