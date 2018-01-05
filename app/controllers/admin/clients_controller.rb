2d252d4a091f76a07ff8e2d21b4551e982775c78
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-04 16:07:37 -0800

    Added admin/clients #index, #show, #update, #destroy

--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -1,3 +1,51 @@
 class Admin::ClientsController < ApplicationController
-  
+  def destroy
+    Client.destroy(params[:id])
+    render json: {
+      message: "Client user deleted",
+    }, status: 200
+  end
+
+  def index
+    render json: {
+      message: "Rendering list of all clients",
+      clients: Client.all,
+    }, status: 200
+  end
+
+  def show
+    id = params[:id]
+    client = Client.find(id)
+    render json: {
+      client_data: client.attributes.except('type', 'password_digest'),
+      job_requests: client.job_requests,
+      jobs: client.jobs,
+    }, status: 200
+  end
+
+  def update
+    id = params[:id]
+    client = Client.find(id)
+    client.update!(filtered_params)
+    render json: {
+      message: "Client data updated",
+      client_data: client.attributes.except('type', 'password_digest')
+    }, status: 200
+  end
+
+  private
+
+  def filtered_params
+    params.permit(
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation,
+      :phone_number,
+      :address,
+      :info,
+    )
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,4 @@
 class Client < User
-  include ActiveModel::AttributeAssignment
-
   has_many :job_requests, class_name: "JobRequest", foreign_key: "client_id"
   has_many :jobs, through: :job_requests
 end
