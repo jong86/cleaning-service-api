8204a7afcdcfd7d4cb30c3bb2c793444a66d0795
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-04 17:35:50 -0800

    Added jobs #index #show for clients

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,15 @@
 class JobsController < ApplicationController
   def index
+    render json: {
+      message: "Rendering list of all current client's jobs",
+      jobs: current_user.jobs,
+    }, status: 200
   end
 
   def show
+    render json: {
+      message: "Rendering requested job data",
+      jobs: current_user.jobs.find(params[:id]),
+    }, status: 200
   end
 end
