e2a870013007087c0a30c079612fcb4d6d20104e
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-18 18:19:18 -0800

    Removed client method from job_request controller

--- app/models/job_request.rb
+++ app/models/job_request.rb
@@ -9,10 +9,4 @@ class JobRequest < ApplicationRecord
   belongs_to :client
   has_many :jobs, class_name: "Job", foreign_key: "job_request_id"
   has_many :interviews, class_name: "Interview", foreign_key: "job_request_id"
-
-  def client
-    if self.client_id
-      User.find(self.client_id)
-    end
-  end
 end
