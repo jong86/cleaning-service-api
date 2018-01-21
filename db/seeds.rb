148e41c10b1e06bfbce338322b6df1d95a65442f
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-20 23:47:39 -0800

    More seed data for employee/clients, and used those user ids to seed job_requests and jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,11 @@
 
 require 'faker'
 
-Employee.create(
-  email: 'e@e.com',
-  password: 'password',
-  password_confirmation: 'password',
-  first_name: 'Fake',
-  last_name: 'Person',
-  username: 'fake_person77',
-  address: '123 Fake St.',
-  phone_number: '123-456-7890',
-  availability: 'Weeknights after 6pm',
-)
 
-Client.create(
-  email: 'c@c.com',
-  password: 'password',
-  password_confirmation: 'password',
-  first_name: 'Another',
-  last_name: 'Guy',
-  username: 'another_guy88',
-  address: '789 Something Drive',
-  phone_number: '999-111-2222',
-)
+
+#
+# Create users
+#
 
 Admin.create(
   email: 'a@a.com',
@@ -33,14 +16,77 @@ Admin.create(
   password_confirmation: 'password',
   first_name: 'Admin',
   last_name: 'User',
-  username: 'admin_user99',
+  username: 'admin_user',
 )
+
+Employee.create(
+  email: 'e@e.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'Employee',
+  last_name: 'User',
+  username: 'employee_user',
+  address: '123 Fake St.',
+  phone_number: '123-456-7890',
+  availability: 'Weeknights after 6pm',
+  info: 'Boring',
+)
+
+employee_user_ids = Array.new
+for i in 0..100
+  employee = Employee.create(
+    email: Faker::Internet.free_email,
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.user_name(4..10),
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    availability: ['Weeknights after 6pm', 'Weekdays from 9-5', 'Weekends all day'].sample,
+    info: Faker::Seinfeld.quote,
+  )
+  employee_user_ids << employee.id
+end
+
+Client.create(
+  email: 'c@c.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'Client',
+  last_name: 'User',
+  username: 'client_user',
+  address: '789 Something Drive',
+  phone_number: '999-111-2222',
+)
+
+client_user_ids = Array.new
+for i in 0..100
+  client = Client.create(
+    email: Faker::Internet.free_email,
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.user_name(4..10),
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    info: Faker::TheFreshPrinceOfBelAir.quote,
+  )
+  client_user_ids << client.id
+end
+
+
+
+#
+# Create job requests
+#
 
 job_request_ids = Array.new
 Faker::Config.locale = 'en-CA'
 for i in 0..100
   job_request = JobRequest.create(
-    client_id: 2,
+    client_id: client_user_ids.sample,
     address: Faker::Address.street_address,
     possible_times: Faker::Date.between(Date.today, 60.days.from_now),
     work_description: Faker::MostInterestingManInTheWorld.quote,
@@ -58,10 +104,15 @@ for i in 0..100
 end
 
 
+
+#
+# Create jobs (no, I'm not a politician)
+#
+
 for i in 0..100
   Job.create(
     job_request_id: job_request_ids.sample,
-    employee_id: 1,
+    employee_id: employee_user_ids.sample,
     confirmed_time: Faker::Time.between(Date.today, 60.days.from_now, :all),
     time_work_started: Faker::Time.between(7.days.from_now, 60.days.from_now, :all),
     time_work_completed: Faker::Time.between(7.days.from_now, 60.days.from_now, :all),
@@ -70,4 +121,3 @@ for i in 0..100
     employee_notes: Faker::HitchhikersGuideToTheGalaxy.marvin_quote,
   )
 end
-
