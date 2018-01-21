f504435fe3044c425976cf11d281cab1f3712397
Jon Gaspar <jon_gaspar@hotmail.com>
2018-01-20 23:24:11 -0800

    Larger database seed, with faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # DB Seed file
 #
 
+require 'faker'
 
 Employee.create(
   email: 'e@e.com',
@@ -35,47 +36,38 @@ Admin.create(
   username: 'admin_user99',
 )
 
-JobRequest.create(
-  client_id: 2,
-  address: '456 Sample Ave.',
-  possible_times: '4pm Saturday',
-  work_description: 'Clean my kitchen please',
-  quantity_hours: 1,
-  interview_requested: false,
-)
+job_request_ids = Array.new
+Faker::Config.locale = 'en-CA'
+for i in 0..100
+  job_request = JobRequest.create(
+    client_id: 2,
+    address: Faker::Address.street_address,
+    possible_times: Faker::Date.between(Date.today, 60.days.from_now),
+    work_description: Faker::MostInterestingManInTheWorld.quote,
+    quantity_hours: Faker::Number.between(1, 8),
+    interview_requested: Faker::Boolean.boolean(0.2),
+    possible_interview_times: Faker::Date.between(Date.today, 14.days.from_now),
+    interview_notes: Faker::Friends.quote,
+    guest_first_name: Faker::Name.first_name,
+    guest_last_name: Faker::Name.last_name,
+    guest_phone_number: Faker::PhoneNumber.phone_number,
+    guest_email: Faker::Internet.free_email,
+    guest_preferred_contact: "email",
+  )
+  job_request_ids << job_request.id
+end
 
-JobRequest.create(
-  client_id: 2,
-  address: '12321 Oak St.',
-  possible_times: '7pm Mondays',
-  work_description: 'Need my bathroom cleaned',
-  quantity_hours: 4.5,
-  interview_requested: true,
-  possible_interview_times: '3:45pm on Sunday',
-  interview_notes: 'Meet me at the door on right side of house',
-)
 
-Job.create(
-  job_request_id: 1,
-  employee_id: 1,
-  confirmed_time: '09/11/18 12:30',
-  admin_notes: 'Do a deep cleaning this time',
-)
+for i in 0..100
+  Job.create(
+    job_request_id: job_request_ids.sample,
+    employee_id: 1,
+    confirmed_time: Faker::Time.between(Date.today, 60.days.from_now, :all),
+    time_work_started: Faker::Time.between(7.days.from_now, 60.days.from_now, :all),
+    time_work_completed: Faker::Time.between(7.days.from_now, 60.days.from_now, :all),
+    is_paid: Faker::Boolean.boolean(0.5),
+    admin_notes: Faker::HitchhikersGuideToTheGalaxy.quote,
+    employee_notes: Faker::HitchhikersGuideToTheGalaxy.marvin_quote,
+  )
+end
 
-Job.create(
-  job_request_id: 2,
-  employee_id: 1,
-  confirmed_time: '09/11/18 12:30',
-  time_work_started: '09/11/18 12:31',
-  time_work_completed: '09/11/18 14:32',
-  is_paid: true,
-  admin_notes: "Don't forget the dishes",
-  employee_notes: 'Bathroom was really dirty',
-)
-
-Interview.create(
-  job_request_id: 2,
-  time: '3:45 Sunday',
-  address: '889 Granville St.',
-  notes: 'Meet on right side of house.',
-)
