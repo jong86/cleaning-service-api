e5f961ca20836a16ee714220879eeff03e8df615
Jon Gaspar <jon_gaspar@hotmail.com>
2018-02-16 13:17:45 -0800

    Sends user data when logging in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,10 @@ class AuthenticationController < ApplicationController
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password], params[:username])
     if command.success?
-      render json: { auth_token: command.result }
+      render json: {
+        auth_token: command.result,
+        user_data: User.find_by_email(params[:email])
+      }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
