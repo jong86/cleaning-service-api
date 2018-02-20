8b01b2d51f0f3204858f819c5694f32edf186074
Jon Gaspar <jon_gaspar@hotmail.com>
2018-02-19 19:27:48 -0800

    Email only sent when bill marked as sent

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -54,24 +54,26 @@ class Admin::JobsController < Admin::AdminController
     job = Job.find(id)
     if job.update!(filtered_params)
 
-      # Send mail w/ link to bill with pony gem
-      require 'pony'
-      Pony.mail({
-        :to => 'vancleaningtestmailer@gmail.com',
-        :via => :smtp,
-        :via_options => {
-          :address              => 'smtp.gmail.com',
-          :port                 => '587',
-          :enable_starttls_auto => true,
-          :user_name            => 'vancleaningservicemailer',
-          :password             => '66hAMzE$2MPp',
-          :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-          :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-        },
-        :from => 'vancleaningservicemailer@gmail.com',
-        :subject => 'Your VanCleaning bill is ready',
-        :html_body => "Thank your for your business.<br/>You may click <a href='http://localhost:8080/billing?uuid=#{job.uuid}'>here</a> to pay your bill.",
-      })
+      if job.bill_sent
+        # Send mail w/ link to bill with pony gem
+        require 'pony'
+        Pony.mail({
+          :to => 'vancleaningtestmailer@gmail.com',
+          :via => :smtp,
+          :via_options => {
+            :address              => 'smtp.gmail.com',
+            :port                 => '587',
+            :enable_starttls_auto => true,
+            :user_name            => 'vancleaningservicemailer',
+            :password             => '66hAMzE$2MPp',
+            :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+            :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+          },
+          :from => 'vancleaningservicemailer@gmail.com',
+          :subject => 'Your VanCleaning bill is ready',
+          :html_body => "Thank your for your business.<br/>You may click <a href='http://localhost:8080/billing?uuid=#{job.uuid}'>here</a> to pay your bill.",
+        })
+      end
 
       # Render response
       render json: {
